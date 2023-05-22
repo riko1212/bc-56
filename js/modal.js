f3266f26b40da47faae344e64fda0b6386313b48
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-05-22 12:47:43 +0300

    mod-7

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,18 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach((el) => {
+    el.addEventListener('click', toggleModal);
+  });
+
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
   }
 })();
-
-console.dir(document.querySelector('.test'));
